b0c41e5ab06a2f210b06fdad5dc11c1d5c94294c
Julian Gomez <juliandr544@gmail.com>
2021-10-07 22:47:01 -0500

    add create

--- Python/django/crudpractice/principal/views.py
+++ Python/django/crudpractice/principal/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from principal.forms import PersonaForm
 from .models import Persona
 # Create your views here.
 def inicio(request):
@@ -6,3 +8,10 @@ def inicio(request):
     return render(request,"index.html",{
         'personas': personas
     })
+
+def crearPersona(request):
+    form = PersonaForm()
+    contexto = {
+        'form':form
+    }
+    return render(request, 'create.html', contexto)
