266730f8dac43b5fa1d3dd8d800de7f182b03535
Archi657 <40327956+Archi657@users.noreply.github.com>
2022-06-12 18:48:23 -0500

    update

---
+++ Java/CarsDB/src/Control/Control.java
@@ -0,0 +1,64 @@
+
+
+package Control;
+
+import Modelo.Revision;
+import Vista.Formulario1;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import javax.swing.table.DefaultTableModel;
+
+
+public class Control implements ActionListener{
+    Formulario1 objVista;
+    Revision objModelo;
+    Revision objCliente;
+    DefaultTableModel modelo = new DefaultTableModel();
+
+    public Control() {
+        objVista = new Formulario1();
+        objVista.setVisible(true);
+        objVista.getjButton1().addActionListener(this);
+        objModelo = new Revision();
+    }
+    
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String nom = objVista.getTxtNombre().getText();
+        String ide = objVista.getTxtIdentificacion().getText();
+        String pla = objVista.getTxtPlaca().getText();
+        String mod = objVista.getTxtModelo().getText();
+        String rev = objVista.getjComboBox1().getSelectedItem().toString();
+       // objModelo.setEstado(rev);
+        
+        objCliente = new Revision(ide, nom, pla, mod, rev);
+        
+        modelo = (DefaultTableModel) objVista.getjTable1().getModel();
+        int filas = objVista.getjTable1().getRowCount();
+        for (int j = 0; j < filas; j++) {
+           modelo.removeRow(0);
+        }
+        
+        modelo = objModelo.adicionar(objCliente);
+        objVista.getjTable1().setModel(modelo);
+        
+        objVista.getTxtRevAprobadas().setText(""+objModelo.getContaprobado());
+        objVista.getTxtRevMantenimiento().setText(""+objModelo.getContmantenimiento());
+        objVista.getTxtRevRechazadas().setText(""+objModelo.getContrechazado());
+        DecimalFormat formato = new DecimalFormat("#,###,###.##");
+        objVista.getTxtMayor().setText("$"+formato.format(objModelo.getMenor()));
+        objVista.getTxtMenor().setText("$"+formato.format(objModelo.getMayor()));
+        objVista.getTxtValorRecaudado().setText("$"+formato.format(objModelo.getContrecaudado()));
+        
+        
+        objVista.getTxtNombre().setText("");
+        objVista.getTxtPlaca().setText("");
+        objVista.getTxtIdentificacion().setText("");
+        objVista.getTxtModelo().setText("");
+        
+    }
+
+}
